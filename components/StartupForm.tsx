a1b8f8184cddc139078d9d801b379a58640259c2
lecaotanloc289 <lecaotanloc289@gmail.com>
2025-09-15 18:42:29 +0700

    feat: sentry - bug

--- components/StartupForm.tsx
+++ components/StartupForm.tsx
@@ -11,6 +11,7 @@ import { formSchema } from "@/lib/validation";
 import z from "zod";
 import { toast } from "sonner";
 import { useRouter } from "next/navigation";
+import { createPitch } from "@/lib/actions";
 
 const StartupForm = () => {
   const [errors, setErrors] = useState<Record<string, string>>({});
@@ -26,15 +27,14 @@ const StartupForm = () => {
         pitch,
       };
       await formSchema.parseAsync(formValues);
-      // const result = await createIdea(prevState, formData, pitch);
-      // // console.log(result);
-      // if (result.status == "SUCCESS") {
-      //   toast("Success", {
-      //     description: "Your startup pitch has been created successfully",
-      //   });
-      //   router.push(`/startup/${result.id}`);
-      // }
-      // return result;
+      const result = await createPitch(prevState, formData, pitch);
+      if (result.status == "SUCCESS") {
+        toast("Success", {
+          description: "Your startup pitch has been created successfully",
+        });
+        router.push(`/startup/${result?._id}`);
+      }
+      return result;
     } catch (error) {
       if (error instanceof z.ZodError) {
         const fieldErrors = error.flatten().fieldErrors;
---
+++ sentry.server.config.ts
@@ -0,0 +1,18 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://21a7f3a3a6e674d61ad3446f6253a326@o4510023144636416.ingest.us.sentry.io/4510023145816064",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
